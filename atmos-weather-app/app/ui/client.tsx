1c194b114a7bf57658442a96e818499f12a800a1
Bruno Rossi <brunogabrielrossi@gmail.com>
2025-02-03 17:44:53 -0500

    Added location name

--- atmos-weather-app/app/ui/client.tsx
+++ atmos-weather-app/app/ui/client.tsx
@@ -1,16 +1,26 @@
 'use client'
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CardsWrapper from "./cards-wrapper";
 import LocationFormWrapper from "./location-form-wrapper";
 import { Location } from "../lib/definitions";
+import { fetchLocations } from "../lib/data";
 
 export default function Client({initialLocations}: { initialLocations: Location[]}) {
 
     const [locations, setLocations] = useState<Location[]>(initialLocations);
 
+    useEffect(() => {
+
+        const loadLocations = async () => {
+            const latestLocations = await fetchLocations();
+            setLocations(latestLocations);
+        };
+        loadLocations();
+    }, []);
+
     return (
-        <div className="flex flex-col row-start-2 items-center sm:items-start">
+        <div className="flex flex-col items-center sm:items-start w-full">
             <LocationFormWrapper setLocations={setLocations} />
             <CardsWrapper locations={locations} setLocations={setLocations} />
         </div>
